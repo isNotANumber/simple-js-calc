63416c622ab7faa75550d1c6e451fe2867ab7fba
Kirill T <nan1911.work@gmail.com>
2024-10-21 19:18:43 +0500

    Math round added to operations

--- src/operations/operations.js
+++ src/operations/operations.js
@@ -1,8 +1,8 @@
 const sum = (a, b) => a + b;
 const minus = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
-const divideWithRemainder = (a, b) => a % b;
+const multiply = (a, b) => Math.round(a * b * 100) / 100;
+const divide = (a, b) => Math.round(a / b * 100) / 100;
+const divideWithRemainder = (a, b) => Math.round(a % b * 100) / 100;
 
 const OPERATIONS = {
   "+": sum,
