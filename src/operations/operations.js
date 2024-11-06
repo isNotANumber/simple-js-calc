4dc0af887d953e180f17bac3022abc97a3c45893
Kirill T <nan1911.work@gmail.com>
2024-11-06 14:56:33 +0500

    operation round bugfix

--- src/operations/operations.js
+++ src/operations/operations.js
@@ -1,8 +1,10 @@
-const sum = (a, b) => a + b;
-const minus = (a, b) => a - b;
-const multiply = (a, b) => Math.round(a * b * 100) / 100;
-const divide = (a, b) => Math.round(a / b * 100) / 100;
-const divideWithRemainder = (a, b) => Math.round(a % b * 100) / 100;
+const roundResult = (value) =>  Math.round(value * 100) / 100;
+
+const sum = (a, b) => roundResult(a + b);
+const minus = (a, b) => roundResult(a - b);
+const multiply = (a, b) => roundResult(a * b);
+const divide = (a, b) => roundResult(a / b);
+const divideWithRemainder = (a, b) => roundResult(a % b);
 
 const OPERATIONS = {
   "+": sum,
